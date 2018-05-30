824653626e13b84f5a46be70eb0b61b3744ab583
Mike Tallerico <mtallerico1@gmail.com>
2018-05-30 07:28:17 -0400

    added start screen function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+const gameBoard = document.getElementById('board');
+const body = document.querySelector('body');
+const div = document.createElement('div');
 
+
+// controlling start screen 
+(function() {
+    body.appendChild(div).setAttribute('id', 'start');
+    const startDiv = document.getElementById('start');
+    startDiv.classList.add('screen', 'screen-start');
+    startDiv.innerHTML = `
+        <header>
+            <h1>Tic Tac Toe</h1>
+            <a href="#" class="button">Start game</a>
+        </header>
+    `;
+    const startButton = document.querySelector('.button');
+    startDiv.addEventListener('click', (e) => {
+        const element = e.target;
+        if(element.tagName === 'A') {
+            startDiv.removeAttribute('class');
+            startDiv.innerHTML = '';
+        }
+    });
+}())
