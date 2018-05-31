5998ce1f9e1244e5e4ab97e902c5f596c32f816c
Mike Tallerico <mtallerico1@gmail.com>
2018-05-31 07:27:44 -0400

    working on adding in player names on start page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 const gameBoard = document.getElementById('board');
 const body = document.querySelector('body');
 const div = document.createElement('div');
+const input = document.createElement('input');
+const label = document.createElement('label');
+const button = document.createElement('a');
+const player1 = {
+    name: '',
+    score: 0 
+};
 
+const player2 = {
+    name: '',
+    score: 0 
+};
+
+
+// ********* HELPER FUNCTIONS ***********************
+// this function sets as many attributes one needs on an element
+function setAttributes(elem) {
+    for (var i = 1; i < arguments.length; i+=2) {
+        elem.setAttribute(arguments[i], arguments[i+1]);
+    }
+}
 
 // controlling start screen 
 (function() {
@@ -9,17 +29,33 @@ const div = document.createElement('div');
     const startDiv = document.getElementById('start');
     startDiv.classList.add('screen', 'screen-start');
     startDiv.innerHTML = `
-        <header>
+        <header id="startHead">
             <h1>Tic Tac Toe</h1>
-            <a href="#" class="button">Start game</a>
+            <div class= "flex_div" id= "playerChoice">
+                <a href="#" class="button" id="onePlayer">1 Player</a>
+                <a href="#" class="button" id="twoPlayer">2 Players</a>
+            </div>
         </header>
     `;
-    const startButton = document.querySelector('.button');
+
+    const choiceContainer = document.getElementById('playerChoice');
+    const headerStart = document.getElementById('startHead');
+
     //listening for click on start game button
     startDiv.addEventListener('click', (e) => {
         const element = e.target;
-        if(element.tagName === 'A') {
-            startDiv.remove();
+        if (element.id === 'onePlayer') {
+            choiceContainer.innerHTML = '';
+            choiceContainer.appendChild(input);
+            choiceContainer.appendChild(button).setAttribute('class', 'button');
+            setAttributes(input,
+                'type', 'text', 
+                'placeholder', 'Player Name',
+                'id', 'playerOneName',
+                'class', 'input'
+            );
+            choiceContainer.classList.remove('flex_div');
+            choiceContainer.classList.add('info_div');
         }
     });
 }())
