a582e665ccb359f2a58c2e800216f2d68856c7ac
Mike Tallerico <mtallerico1@gmail.com>
2018-06-01 08:14:04 -0400

    added buttons for players and name inputs for player names

--- js/app.js
+++ js/app.js
@@ -6,12 +6,14 @@ const label = document.createElement('label');
 const button = document.createElement('a');
 const player1 = {
     name: '',
-    score: 0 
+    score: 0, 
+    turn: false
 };
 
 const player2 = {
     name: '',
-    score: 0 
+    score: 0,
+    turn: true
 };
 
 
@@ -22,6 +24,12 @@ function setAttributes(elem) {
         elem.setAttribute(arguments[i], arguments[i+1]);
     }
 }
+
+// show/hide element
+function showHideEl(el, displayType) {
+    el.style.display = displayType;
+}
+
 
 // controlling start screen 
 (function() {
@@ -34,28 +42,48 @@ function setAttributes(elem) {
             <div class= "flex_div" id= "playerChoice">
                 <a href="#" class="button" id="onePlayer">1 Player</a>
                 <a href="#" class="button" id="twoPlayer">2 Players</a>
+                <input type='text' id='playerOneName' class="input" placeholder='Player One Name'>
+                <input type='text' id='playerTwoName' class="input" placeholder='Player Two Name'>
+                <a href="#" class="button" id="start_button">Start Game</a>
             </div>
         </header>
     `;
-
+    
     const choiceContainer = document.getElementById('playerChoice');
     const headerStart = document.getElementById('startHead');
+    const startButton = document.getElementById('start_button');
+    const playerOneButton = document.getElementById('onePlayer');
+    const playerTwoButton = document.getElementById('twoPlayer');
+    const playerOneInput = document.getElementById('playerOneName');
+    const playerTwoInput = document.getElementById('playerTwoName');
+    //initially hiding the start button until needed
+    showHideEl(startButton, 'none');
+    showHideEl(playerOneInput, 'none');
+    showHideEl(playerTwoInput, 'none');
 
     //listening for click on start game button
     startDiv.addEventListener('click', (e) => {
         const element = e.target;
         if (element.id === 'onePlayer') {
-            choiceContainer.innerHTML = '';
-            choiceContainer.appendChild(input);
-            choiceContainer.appendChild(button).setAttribute('class', 'button');
-            setAttributes(input,
-                'type', 'text', 
-                'placeholder', 'Player Name',
-                'id', 'playerOneName',
-                'class', 'input'
-            );
-            choiceContainer.classList.remove('flex_div');
-            choiceContainer.classList.add('info_div');
+            showHideEl(playerOneButton, 'none');
+            showHideEl(playerTwoButton, 'none');
+            showHideEl(playerOneInput, 'block');
+            startButton.removeAttribute('style');
+            player2.name = 'computer'
+        }
+        if (element.id === 'twoPlayer') {
+            showHideEl(playerOneButton, 'none');
+            showHideEl(playerTwoButton, 'none');
+            showHideEl(playerOneInput, 'block');
+            showHideEl(playerTwoInput, 'block');
+            startButton.removeAttribute('style');
+        }
+        if (element.id === 'start_button') {
+            console.log(playerOneInput.value.length);
+            if(playerOneInput.value.length > 0 && player2.name === 'computer') {
+                player1.name = playerOneInput.value;
+                startDiv.remove();
+            }
         }
     });
 }())
